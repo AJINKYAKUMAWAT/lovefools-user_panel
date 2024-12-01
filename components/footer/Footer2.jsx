e03939ca62b4a1bdec1a22a267d8affdce506838
Ajinkya Kumawat <ajink3994@gmail.com>
2024-12-02 01:58:31 +0530

    issue fixes

--- components/footer/Footer2.jsx
+++ components/footer/Footer2.jsx
@@ -1,8 +1,9 @@
-import { Divider, Grid, Typography, Container } from '@mui/material';
-import Image from 'next/image';
-import React from 'react';
-import logo from '../../assets/images/logo2.png';
-import { Facebook, Instagram, Twitter } from '@mui/icons-material';
+import { Divider, Grid, Typography, Container } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import logo from "../../assets/images/logo2.png";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { AuthContextProvider } from "@/authcontext/AuthContext";
 
 const navItems = [
   // "Home",
@@ -12,60 +13,54 @@ const navItems = [
   "Event",
   "Gallery",
   "Testimonial",
-  "Contact us"
+  "Contact us",
 ];
 
 const Footer2 = () => {
+  const { id, setId, setEnquiryName } = React.useContext(AuthContextProvider);
+
   return (
-    <footer className='common-footer'>
+    <footer className="common-footer">
       <Container className="footer-container">
         <Grid container>
-        <Grid item xs={12} md={12}>
-        <Image src={logo} width={150} height={50} alt="Company Logo" />
-        </Grid>
+          <Grid item xs={12} md={12}>
+            <Image src={logo} width={150} height={50} alt="Company Logo" />
+          </Grid>
           <Grid item xs={12} md={5}>
             <Typography>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             </Typography>
           </Grid>
-          
-          <Grid
-            item
-            xs={12} 
-            md={12}
-            lg={7}  >
+
+          <Grid item xs={12} md={12} lg={7}>
             <div className="footer-menu">
-            {navItems.map((i, index) => {
-              return (
-                <a 
-                  key={index}>
-                  {i}
-                </a>
-              );
-            })}
+              {navItems.map((i, index) => {
+                return (
+                  <div onClick={() => setId(i)}>
+                    <a href={`#${id}`} key={index}>
+                      {i}
+                    </a>
+                  </div>
+                );
+              })}
             </div>
           </Grid>
         </Grid>
-        <Divider className='common-divider'/>
+        <Divider className="common-divider" />
         <Grid container alignItems="center">
           <Grid item xs={12} md={5}>
-            <Typography>
-              © 2024 Lovefools. All Rights Reserved.
-            </Typography>
+            <Typography>© 2024 Lovefools. All Rights Reserved.</Typography>
           </Grid>
 
-          <Grid item
-            xs={12} 
-            md={12}
-            lg={7} >
-                <div className="footer-menu">
-            <Facebook />
-            <Twitter />
-            <Instagram />
+          <Grid item xs={12} md={12} lg={7}>
+            <div className="footer-menu">
+              <Facebook />
+              <Twitter />
+              <Instagram />
             </div>
           </Grid>
         </Grid>
-        </Container>
+      </Container>
     </footer>
   );
 };
