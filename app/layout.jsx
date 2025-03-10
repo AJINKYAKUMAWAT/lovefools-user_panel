1704cd56bf66d983b8d41274641065c6bfbd7897
Ajinkya Kumawat <ajink3994@gmail.com>
2025-03-10 21:43:54 +0530

    add head content

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,46 +18,54 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>LoveFools</title>
-        <link rel="icon" type="image/x-icon" href="/icon.png" />
-        <meta name="title" content="LoveFools" />
-        <meta
-          name="description"
-          content="Menu - Check out the Menu of The LoveFools"
-        />
+        <meta charset="utf-8" />
+        <title>LOVEFOOLS HOSPITALITY PRIVATE LIMITED</title>
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="LoveFools" />
-        <meta
-          property="og:description"
-          content="Menu - Check out the Menu of The LoveFools"
-        />
-        <meta
-          property="og:url"
-          content="https://lovefools-user-panel.vercel.app/"
-        />
+        <meta property="og:url" content="https://thelovefools.in" />
         <meta
           property="og:image"
-          content={`https://lovefools-user-panel.vercel.app/assets/images/og-tag.jpg`}
+          content="https://the-lovefools.s3.eu-north-1.amazonaws.com/uploads/67507d2e7218dcbcba25e5ef-.jpg"
         />
         <meta
           property="og:image:secure_url"
-          content={`https://lovefools-user-panel.vercel.app/assets/images/og-tag.jpg`}
+          content="https://the-lovefools.s3.eu-north-1.amazonaws.com/uploads/67507d2e7218dcbcba25e5ef-.jpg"
         />
-        <meta property="og:image:alt" content="LoveFools" />
-        <meta property="og:image:type" content="image/svg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="https://thelovefools.in" />
+        <meta property="og:image:type" content="image/png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="LoveFools" />
-        <meta
-          name="twitter:description"
-          content="Menu - Check out the Menu of The LoveFools"
-        />
         <meta
           name="twitter:image"
-          content={`https://lovefools-user-panel.vercel.app/assets/images/og-tag.jpg`}
+          content="https://the-lovefools.s3.eu-north-1.amazonaws.com/uploads/67507d2e7218dcbcba25e5ef-.jpg"
         />
         <meta
           name="twitter:image:secure_url"
-          content={`https://lovefools-user-panel.vercel.app/assets/images/og-tag.jpg`}
+          content="https://the-lovefools.s3.eu-north-1.amazonaws.com/uploads/67507d2e7218dcbcba25e5ef-.jpg"
+        />
+        <meta
+          property="og:title"
+          content="LOVEFOOLS HOSPITALITY PRIVATE LIMITED"
+        />
+        <meta
+          name="twitter:title"
+          content="LOVEFOOLS HOSPITALITY PRIVATE LIMITED"
+        />
+        <meta
+          name="keywords"
+          content="keywords: digital,design/development studio, ui/ux studio, ux studio,development studio,design and development studio,product design and development studio,growth hacks for startups,growth strategies in marketing"
+        />
+        <meta
+          name="description"
+          content="og:description : LOVEFOOLS HOSPITALITY PRIVATE LIMITED "
+        />
+        <meta
+          property="og:description"
+          content="og:description : LOVEFOOLS HOSPITALITY PRIVATE LIMITED "
+        />
+        <meta
+          name="twitter:description"
+          content="og:description : LOVEFOOLS HOSPITALITY PRIVATE LIMITED "
         />
       </head>
       <body className="lovefools-body">
